55f2e3caea15ef1c8d54a5dcd594138a4dd2b6cf
Rajin <rajink92@gmail.com>
2022-03-04 16:25:42 -0500

    breed to temportate literal, add edge case

--- w5/api/breedFetcher.js
+++ w5/api/breedFetcher.js
@@ -2,8 +2,13 @@ const require = ('request');
 
 module.exports = (breed, callback) => {
   const options = {
-    url: `https://api.thecatapi.com/v1/breeds/search?q=sib`,
+    url: `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
     headers: {
       'x-api-key': '7e2cb31f-5f9d-4b4b-b872-dd2b2b7a2b9a'
     }
   }
+  request(options, (err, resp, body) => {
+    if (!err) {
+  body = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
