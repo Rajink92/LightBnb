da98aa4c3d7c052d7f939f3c8b21f6430da658e5
Rajin <rajink92@gmail.com>
2022-03-04 16:39:35 -0500

    linted

--- w5/api/breedFetcher.js
+++ w5/api/breedFetcher.js
@@ -1,4 +1,4 @@
-const require = ('request');
+const request = require('request');
 
 module.exports = (breed, callback) => {
   const options = {
@@ -6,9 +6,31 @@ module.exports = (breed, callback) => {
     headers: {
       'x-api-key': '7e2cb31f-5f9d-4b4b-b872-dd2b2b7a2b9a'
     }
-  }
+  };
   request(options, (err, resp, body) => {
     if (!err) {
-  body = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+      if (resp.statusCode !== 200) {
+        return callback(`Could not connect to the server. statusCode ${resp.statusCode}`);
+      } else {
+        if (body) {
+          body = JSON.parse(body);
+          // console.log(data);
+          // console.log(typeof data);
+          if (body.length) {
+            if (body[0].description) {
+              return callback(null, body[0].description);
+            } else {
+              return callback(`No description for this breed, ${breed}, is provided.`);
+            }
+          } else {
+            return callback(`Could not find that breed, ${breed}.`);
+          }
+        } else {
+          return callback(`Recieved empty response from server`);
+        }
+      }
+    } else {
+      return callback(`Could not fetch info about '${breed}' cat breed from the server.`);
+    }
+  });
+};
